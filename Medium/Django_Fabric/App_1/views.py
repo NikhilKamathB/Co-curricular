f5f61d7b2d0378d7a484f3409aee5534cf055416
Nikhil Kamath B <nikhilkb98@gmail.com>
2021-02-16 23:17:10 +0530

    Added html pages

--- Medium/Django_Fabric/App_1/views.py
+++ Medium/Django_Fabric/App_1/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'App_1/home.html', {})
+    context = {'entries': range(1, 5)}
+    return render(request, 'App_1/home.html', context)

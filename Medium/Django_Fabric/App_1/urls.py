9617a2a44e733d9fa2fba956406d85a87877c5da
Nikhil Kamath B <nikhilkb98@gmail.com>
2021-02-17 18:57:24 +0530

    Added hands, pose and holistic

--- Medium/Django_Fabric/App_1/urls.py
+++ Medium/Django_Fabric/App_1/urls.py
@@ -7,4 +7,7 @@ app_name = 'app_1'
 urlpatterns = [
     path('', views.home, name='home'),
     path('face-mesh/', views.face_mesh, name='face_mesh'),
+    path('hands/', views.hands, name='hands'),
+    path('pose/', views.pose, name='pose'),
+    path('holistic/', views.holistic, name='holistic'),
 ]
--- Medium/Django_Fabric/App_1/views.py
+++ Medium/Django_Fabric/App_1/views.py
@@ -5,6 +5,22 @@ def home(request):
     context = {}
     return render(request, 'App_1/home.html', context)
 
+
 def face_mesh(request):
     context = {}
     return render(request, 'App_1/face_mesh.html', context)
+
+
+def hands(request):
+    context = {}
+    return render(request, 'App_1/hands.html', context)
+
+
+def pose(request):
+    context = {}
+    return render(request, 'App_1/pose.html', context)
+
+
+def holistic(request):
+    context = {}
+    return render(request, 'App_1/holistic.html', context)

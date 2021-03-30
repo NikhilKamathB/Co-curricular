1abd7429549b3308067e083f386e08cbd43615d2
Nikhil Kamath B <nikhilkb98@gmail.com>
2021-03-30 11:10:45 +0530

    Initiated EDA on dataset

--- Medium/Django_Admin_Graph/App_1/admin.py
+++ Medium/Django_Admin_Graph/App_1/admin.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import plotly.io as pio
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 from .models import *
 from django.contrib import admin
 
@@ -5,6 +9,29 @@ from django.contrib import admin
 class BostonHousePriceDatasetAdmin(admin.ModelAdmin):
     
     list_display = ["id", "crim", "zn", "indus", "chas", "nox", "rm", "age", "dis", "rad", "tax", "ptratio", "b", "lstat", "price"]
+
+    def changelist_view(self, request, extra_context=None):
+        df = pd.DataFrame.from_records(BostonHousePriceDataset.objects.all().values_list("crim", "zn", "indus", "chas", "nox", "rm", "age", "dis", "rad", "tax", "ptratio", "b", "lstat", "price", "created_at"))
+        df.columns = self.list_display[1: ] + ["created_at"]
+        df.drop("created_at", inplace=True, axis=1)
+        corr = df.drop("price", axis=1).corr()
+
+        fig = make_subplots(
+                rows=1,
+                cols=2,
+                specs=[
+                        [{"colspan": 1}, {"colspan": 1}]
+                    ],
+                subplot_titles=("Price Distribution", "Correlation between attributes")
+            )
+        fig.add_trace(go.Histogram(x=df["price"]), row=1, col=1)
+        fig.add_trace(go.Heatmap({'x': corr.index.values, 'y': corr.columns.values, 'z': corr.values}), row=1, col=2)
+
+        fig.update_layout(height=500, title_text="EDA on Boston House Price Prediction Dataset")
+        plot = pio.to_html(fig, full_html=False)
+        extra_context = extra_context or {"plot": plot}
+        return super().changelist_view(request, extra_context=extra_context)
+
 
 
 admin.site.register(BostonHousePriceDataset, BostonHousePriceDatasetAdmin)

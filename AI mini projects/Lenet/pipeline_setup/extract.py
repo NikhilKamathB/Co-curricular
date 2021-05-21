5e218c1feec83054c24544c775430b607d4474f4
Nikhil Kamath B <nikhilkb98@gmail.com>
2021-05-21 14:06:21 +0530

    Updated ml pipeline setup and gitignore file

--- AI mini projects/Lenet/pipeline_setup/extract.py
+++ AI mini projects/Lenet/pipeline_setup/extract.py
@@ -1,6 +1,9 @@
+import os
 import torch
 import numpy as np
 from easydict import EasyDict 
 
 if __name__=='__main__':
     print("If you see this text/message, it means that the execution has happened successfully")
+    print(f"Current working directory -> {os.getcwd()}")
+    print(f"List of files in the current working directory ->\n{os.listdir(os.getcwd())}")

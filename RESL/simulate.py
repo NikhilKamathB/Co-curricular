fbfc5858f4b889fb4a530ff2f087ebdec83c4133
Nikhil Kamath B <nikhilkb98@gmail.com>
2022-12-18 02:13:37 -0800

    Edited RESL simulation file and commited IOS app 1

--- RESL/simulate.py
+++ RESL/simulate.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 t = 1000 # milliseconds
 world = (100, 100)
+repulsion_strength = 10
 h_list = list(set([_*(np.pi/180) for _ in range(0, 360, 30)] + [_*(np.pi/180) for _ in range(45, 360, 45)]))
 
 min_velocity_magnitude_player, max_velocity_magnitude_player = 0, 1
@@ -49,26 +50,20 @@ def interaction_model(p_player:tuple, v_player:tuple, p_ego:tuple, v_ego:tuple) 
            v_ego - velocity vector of the ego in the local worl
     output: tuple of distance to ego, actual position and actual velocity vector
     '''
-    next_p_player = []
-    h_player = np.arctan2(v_player[1], v_player[0])
-    v_player_magnitude = np.linalg.norm(v_player)
-    if h_player < 0: h_player = 2*np.pi + h_player
-    r_h_player = [_ % (2*np.pi) if _ >= 2*np.pi else _ for _ in np.linspace(0, 2*np.pi, 36)] + [h_player]
     next_p_ego = (p_ego[0]+(v_ego[0]*t*1e-3), p_ego[1]+(v_ego[1]*t*1e-3))
-    for heading in r_h_player:
-        x_hat, y_hat = v_player_magnitude*np.cos(heading), v_player_magnitude*np.sin(heading)
-        p_player_dash = (p_player[0]+(x_hat*t*1e-3), p_player[1]+(y_hat*t*1e-3))
-        if p_player_dash[0] >= 0 and p_player_dash[0] <= world[0] and p_player_dash[1] >= 0 and p_player_dash[1] <= world[1]:
-            d_player_ego = distance(p_player_dash, next_p_ego)
-            next_p_player.append((d_player_ego, p_player_dash, (x_hat, y_hat)))
-    if not next_p_player:
-        p_player_dash = (min(max(0, p_player[0]), world[0]), min(max(0, p_player[1]), world[1]))
-        h_player = np.arctan2(p_player[1]-p_player_dash[1], p_player[0]-p_player_dash[0])
-        x_hat, y_hat = v_player_magnitude*np.cos(heading), v_player_magnitude*np.sin(heading)
-        d_player_ego = distance(p_player_dash, next_p_ego)
-        next_p_player.append((d_player_ego, p_player_dash, (x_hat, y_hat)))
-    next_p_player = sorted(next_p_player, key=lambda x: x[0], reverse=True) 
-    return next_p_player[0]
+    d_player_ego = distance(p_player, next_p_ego)
+    h_repulsion = (p_player[0]-next_p_ego[0], p_player[1]-next_p_ego[1])
+    repulsion_scale = repulsion_strength/(d_player_ego**2)
+    x_hat, y_hat = v_player[0]+(repulsion_scale*h_repulsion[0]), v_player[1]+(repulsion_scale*h_repulsion[1])
+    next_p_player = (p_player[0]+(x_hat*t*1e-3), p_player[1]+(y_hat*t*1e-3))
+    if next_p_player[0] < 0 or next_p_player[1] < 0 or next_p_player[0] > world[0] or next_p_player[1] > world[1]:
+        next_p_player = (min(max(0, next_p_player[0]), world[0]), min(max(0, next_p_player[1]), world[1]))
+        v_player_magnitude = np.linalg.norm((next_p_player[0]-p_player[0], next_p_player[1]-p_player[1]))
+        h_player = np.arctan2(p_player[1]-next_p_player[1], p_player[0]-next_p_player[0])
+        x_hat, y_hat = v_player_magnitude*np.cos(h_player), v_player_magnitude*np.sin(h_player)
+        d_player_ego = distance(next_p_player, next_p_ego)
+    return (d_player_ego, next_p_player, (x_hat, y_hat))
+
 
 def heuristic(p_player:tuple, p_ego:tuple) -> float:
     '''
@@ -158,22 +153,39 @@ def simulate():
     '''
     p_player_plot = []
     v_player_plot = []
-    p_player, p_heading = player_state, player_heading
-    p_ego, v_ego = ego_state, [np.cos(ego_heading), np.sin(ego_heading)]
+    # p_player, p_heading = player_state, player_heading
+    # p_ego, v_ego = ego_state, [np.cos(ego_heading), np.sin(ego_heading)]
+    goal_state = (90, 90)
+    p_player, p_heading = [50, 50], np.pi
+    p_ego, v_ego = [90, 90], [np.cos(0), np.sin(0)]
     reached_goal_state = False
     ctr = 0
-    while not reached_goal_state:
-        ctr += 1
-        p_player_previous = p_player
-        p_player_plot.append(p_player_previous)
-        v_player_plot.append(p_ego)
-        _, v_player = movement_model(p_player, p_heading)
-        _, p_player, v_player = interaction_model(p_player, v_player, p_ego, v_ego)
-        p_heading = np.arctan2(v_player[1], v_player[0])
-        p_ego, v_ego = ego_model(p_player, p_player_previous, p_ego)
-        if ((p_player[0]-goal_state[0])**2 + (p_player[1]-goal_state[1])**2)**0.5 < goal_aware_sphere_radius:
-            reached_goal_state=True
-        print(f'Step {ctr}: Distance to goal state:\tFrom player = {distance(p_player, goal_state)}\tFrom ego = {distance(p_ego, goal_state)}')
+    try:
+        while not reached_goal_state:
+            ctr += 1
+            p_player_previous = p_player
+            p_player_plot.append(p_player_previous)
+            v_player_plot.append(p_ego)
+            _, v_player = movement_model(p_player, p_heading)
+            _, p_player, v_player = interaction_model(p_player, v_player, p_ego, v_ego)
+            p_heading = np.arctan2(v_player[1], v_player[0])
+            p_ego, v_ego = ego_model(p_player, p_player_previous, p_ego)
+            if ((p_player[0]-goal_state[0])**2 + (p_player[1]-goal_state[1])**2)**0.5 < goal_aware_sphere_radius:
+                reached_goal_state=True
+            print(f'Step {ctr}: Distance to goal state:\tFrom player = {distance(p_player, goal_state)} with velocity vector = {v_player}\tFrom ego = {distance(p_ego, goal_state)} with velocity vector = {v_ego}')
+    except KeyboardInterrupt as E:
+        x = np.asarray(p_player_plot)
+        y = np.asarray(v_player_plot)
+        plt.figure(figsize=(13, 7))
+        plt.xlim(0, world[0])
+        plt.ylim(0, world[0])
+        plt.grid()
+        plt.plot(x[:, 0], x[:, 1], '--bo', label='line with marker')
+        plt.plot(y[:, 0], y[:, 1], '--ro', label='line with marker')
+        plt.plot(goal_state[0], goal_state[1], marker="o", markersize=10,markerfacecolor="green")
+        plt.plot(x[-1,0], x[-1, 1], marker="*", markersize=10,markerfacecolor="red")
+        plt.plot(y[-1,0], y[-1, 1], marker="*", markersize=10,markerfacecolor="red")
+        plt.show()
     x = np.asarray(p_player_plot)
     y = np.asarray(v_player_plot)
     plt.figure(figsize=(13, 7))
